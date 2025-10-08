c99864f673c05e367668fb4cdd01c5b5cc1eb476
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-10-08 21:25:10 +0530

    Saved data with JPA

--- src/main/java/com/microservice/crud/CrudApplication.java
+++ src/main/java/com/microservice/crud/CrudApplication.java
@@ -1,5 +1,7 @@
 package com.microservice.crud;
 
+import com.microservice.crud.dao.StudentDAO;
+import com.microservice.crud.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,20 @@ public class CrudApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("📀 Hello MicroService");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("🫘 Creating the student.");
+        Student tempStudent = new Student("Akash", "Ramjyothi", "akash.ramjyothi@gmail.com");
+
+        System.out.println("💾 Saving student.");
+        studentDAO.save(tempStudent);
+
+        System.out.println("🫆 Displaying Student ID: " + tempStudent.getId());
     }
 
 }
